76665dcc3c37223d728f48ed929852ab5a1cce18
Lovely Sharma <lovelysingeras297@gmail.com>
2024-01-03 20:53:03 +0530

    Added view resume button

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -18,15 +18,13 @@ const Cover: FC<CoverProps> = ({}) => {
                         <Link
                             className="hidden md:block"
                             target="_blank"
-                            href="https://instagram.com/lovely_sharma0812"
+                            href="https://drive.google.com/file/d/1FFJMkW9o5u2ZxmU5UTCT9PpKn_TN8-RJ/view?usp=sharing"
                         >
                             <Button variant="primary" sizes={'small'}>
-                                <Instagram />
-                                <a href="https://drive.google.com/file/d/1FFJMkW9o5u2ZxmU5UTCT9PpKn_TN8-RJ/view?usp=sharing">
-                                    <span className="hidden md:block">
-                                        View Resume
-                                    </span>
-                                </a>
+                                {/* <Instagram /> */}
+                                <span className="hidden md:block">
+                                    View Resume
+                                </span>
                             </Button>
                         </Link>
                     </div>
